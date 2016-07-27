f478dd7bb580612b49c9849f9391b2a108d5b1f0
Isratmity01 <mity.crystal@gmail.com>
2016-07-28 02:31:47 +0600

    working!

--- app/src/main/java/com/example/hp/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hp/myapplication/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.hp.myapplication;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.webkit.JavascriptInterface;
@@ -25,30 +29,69 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         browser = (WebView)findViewById(R.id.webView);
+
         browser.getSettings().setJavaScriptEnabled(true);
         browser.getSettings().setLoadsImagesAutomatically(true);
-        browser.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-        browser.setWebViewClient(new MyBrowser());
-        browser.loadUrl("file:///android_asset/trial.html");
-    }
+        browser.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+
+      //  browser.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+      //  browser.setWebViewClient();
+        browser.getSettings().setBlockNetworkLoads(true);
+
+        browser.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                browser.clearHistory();
+                super.onPageFinished(browser, "about:blank");
+                Toast.makeText(getApplicationContext(), "Done!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(getApplicationContext(), "Oh no! " + description, Toast.LENGTH_SHORT).show();
+            }
+        });
+        browser.loadUrl("file:///android_asset/map.html");
 
 
-    private class MyBrowser extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            view.addJavascriptInterface(new Object()
-            {
-                @JavascriptInterface
-                public void performClick() throws Exception
-                {
-                    Log.d("LOGIN::", "Clicked");
-                    Toast.makeText(MainActivity.this, "Login clicked", Toast.LENGTH_LONG).show();
+        browser.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return (event.getAction() == MotionEvent.ACTION_MOVE);
+            }
+        });
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+
+            }
+        }, 4000);
+        Handler handler2 = new Handler();
+        handler2.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (Build.VERSION.SDK_INT < 18) {
+                    browser.clearView();
+                } else {
+                    browser.loadUrl("about:blank");
                 }
-            }, "login");
-            return true;
-        }
+                browser.clearHistory();
+
+                finish();
+                startActivity(new Intent(MainActivity.this, OpeningActivity.class));
+                //overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                //overridePendingTransition(0, 0);
+
+
+            }
+        }, 4500);
+
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/com/example/hp/myapplication/OpeningActivity.java
@@ -0,0 +1,15 @@
+package com.example.hp.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by HP on 7/27/2016.
+ */
+public class OpeningActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a);
+    }
+}
